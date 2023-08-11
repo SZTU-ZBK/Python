c0028765719618ea758f5bae942d6c0c5b739659
SZTU-ZBK <3068373705@qq.com>
2023-08-11 23:57:58 +0800

    delete test.py

--- code/day_01/washOption.py
+++ code/day_01/washOption.py
@@ -3,7 +3,7 @@ class Washer():
         print("Washing now!!!")
     def Print_Info(self):
         print(f'洗衣机高度{self.width}')
-        print(f'洗衣机高度{self.height}') #可以在类的内部通过self直接获取在类外部定义的属性值
+        print(f'洗衣机高度{self.height}') #可以在类的内部通过self直接获取在类外部定义的属性
 haier_1 = Washer()
 haier_1.width = 500
 haier_1.height = 400
