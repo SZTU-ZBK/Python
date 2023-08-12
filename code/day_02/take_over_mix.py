34b51529243a377b317cc9f395c1eeb7409e90ee
SZTU-ZBK <3068373705@qq.com>
2023-08-12 16:28:50 +0800

    update

---
+++ code/day_02/take_over_mix.py
@@ -0,0 +1,23 @@
+class Master(object):
+    def __init__(self):
+        self.kongfu = '古法煎饼果子'
+    def make_cake(self):
+        print(f'运用{self.kongfu}制作煎饼果子')
+class School(object):
+    def __init__(self):
+        self.kongfu = '黑马煎饼方法'
+    def make_cake(self):
+        print(f'运用{self.kongfu}制作煎饼果子')
+class Prentice(Master,School):               #当两个父类由同名的属性和方法时，子类优先继承第一个父类的同名属性和方法
+    def __init__(self):
+        self.__init__()                      #防止子类的属性被父类覆盖
+        self.kongfu = '独创煎饼方法'          #子类可以通过对父类同名属性或同名方法进行重新定义，来实现类的重写
+    def make_cake(self):
+        self.__init__()
+        print(f'运用{self.kongfu}制作煎饼果子')
+    def Master_make_cake(self):
+        Master.__init__(self)                #这里想要调用父类的同名属性和方法，所以这里需要再次调用父类的初始化。
+        Master.make_cake(self)
+test = Prentice()
+test.make_cake()
+test.Master_make_cake()
